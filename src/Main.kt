559480f46842e37d77bc533e4daa8293531cadd9
JorgeHe22 <jorgex.herrera24@gmail.com>
2025-02-28 01:29:39 -0500

    edificio

--- src/Main.kt
+++ src/Main.kt
@@ -1,113 +1,44 @@
+import java.util.Scanner
 fun main() {
 
-    var moneda20=0
-    var moneda50=0
-    var moneda100=0
-    var moneda200=0
-    var moneda500=0
-    var continuar = true
+    val scanner = Scanner(System.`in`)
+    var intentosFallidos = 0
+    val intentosMax = 3
 
+    while (intentosFallidos < intentosMax) {
+        println("Bienvenido, para acceder al edificio ingrese su codigo de acceso: ")
 
-    while (continuar) {
-        println(
-            """
-        ¿Que deseas hacer?
-        1. Agregar monedas
-        2. Contar monedas
-        3. Calcular el dinero ahorrado
-        4. Vaciar la alcancia 
-        5. Salir al menú
-    """.trimIndent()
-        )
+        val codigo = scanner.nextInt()
 
-        println("Elgie una opcion: ")
-        when (readLine()?.toIntOrNull()){
-            1 -> {
-                var seguirAgregando = true
+        when {
+            codigo in 1000..1999 -> {
+                println("¡Codigo aceptado! Bienvenido empleado #$codigo")
+                break
+            }
 
-                while (seguirAgregando) {
-                    println(
-                        """
-                     Ingresa el valor de la moneda: 
-                        1. Moneda de 20
-                        2. Moneda de 50
-                        3. Moneda de 100
-                        4. Moneda de 200
-                        5. Moneda de 500
-                        
-                    """.trimIndent()
-                    )
+            codigo in 2000..2999 -> {
+                println("Codigo de visitante, por favor complete el registro: ")
+                scanner.nextLine()
 
+                println("Ingrese su nombre: ")
+                val nombre = scanner.nextLine()
 
-                    println("Elige una opcion: ")
-                    when (readLine()?.toIntOrNull()) {
-                        1 -> {
-                            moneda20++
-                            println("Se agrego la moneda de 20")
-                        }
+                println("Cual es el motivo de su visita: ")
+                val motVist = scanner.nextLine()
 
-                        2 -> {
-                            moneda50++
-                            println("Se agrego la moneda de 50")
-                        }
+                println("Registro completo. Bienvenido $nombre. Motivo: $motVist")
+                break
+            }
 
-                        3 -> {
-                            moneda100++
-                            println("Se agrego la moneda de 100")
-                        }
+            else -> {
+                intentosFallidos++
+                println("Codigo incorrecto. Intento: $intentosFallidos\nNumero maximo de intentos: $intentosMax")
 
-                        4 -> {
-                            moneda200++
-                            println("Se agrego la moneda de 200")
-                        }
-
-                        5 -> {
-                            moneda500++
-                            println("Se agrego la moneda de 500")
-                        }
-
-                        6 -> {
-                            seguirAgregando = false
-                            println("Volviendo al menú principal...")
-                        }
-
-                        else -> println("Opcion no valida. Por favor intentar de nuevo")
-
-                    }
+                if (intentosFallidos >= intentosMax) {
+                    println("Ha excedido el numero maximo de intentos.")
                 }
-
-            }
-            2 -> {
-                println("\n Conteo de tus monedas")
-                println("Monedas de 20: $moneda20")
-                println("Mondesas de 50: $moneda50")
-                println("Monedas de 100: $moneda100")
-                println("Monedas de 200: $moneda200")
-                println("Monedas de 500: $moneda500")
-                println("Total de monedas: ${moneda20 + moneda50 + moneda100 + moneda200 + moneda500}")
-            }
-            3 -> {
-                val total = (moneda20 * 20) + (moneda50 * 50) + (moneda100 * 100) + (moneda200 * 200) + (moneda500 * 500)
-                println("\n Total dinero ahorrado")
-                println("Total Monedas de 20: ${moneda20*20}")
-                println("Total Monedas de 50: ${moneda50*50}")
-                println("Total Monedas de 100: ${moneda100*100}")
-                println("Total Monedas de 200: ${moneda200*200}")
-                println("Total Monedas de 500: ${moneda500*500}")
-                println("Total de Dinero Ahorrado: $total")
-            }
-            4 -> {
-                println("Vaciando la alcancia...")
-                moneda20 = 0
-                moneda50 = 0
-                moneda100 = 0
-                moneda200 = 0
-                moneda500 = 0
-            }
-            5 -> {
-                println("Saliendo al Menú")
-                continuar=false
             }
         }
+
     }
 }
